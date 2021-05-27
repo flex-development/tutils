8a8b8e4d0898929ccc78daa21164a8f6041c5060
Lexus Drumgold <lex@flexdevelopment.llc>
2021-05-27 17:13:42 -0400

    feat(types): `DeepPartialByRequired`

---
+++ src/types/DeepPartialByRequiredHelper.ts
@@ -0,0 +1,27 @@
+import type { ObjectPlain } from './ObjectPlain'
+import type { Path } from './Path'
+
+/**
+ * @file Types - DeepPartialByRequiredHelper
+ * @module types/DeepPartialByHelper
+ */
+
+/**
+ * `DeepPartialByRequired` helper.
+ *
+ * @template T - Object type
+ * @template P - Required object field paths
+ * @template F - Object field path filter object. Omits fields
+ *
+ * @example
+ *  interface IPerson { ... }
+ *  type Helper = DeepPartialByHelper<IPerson, 'firstName', { age: never }>
+ */
+export type DeepPartialByRequiredHelper<
+  T extends ObjectPlain,
+  P extends Path<T>,
+  F extends Partial<Record<Path<T>, never>> = {}
+> = {
+  omit: F
+  pick: P
+}
