51df3109f7868c4c510eca66d6e55e38dae3033f
Lexus Drumgold <unicornware@flexdevelopment.llc>
2021-09-04 14:23:59 -0400

    style: fix linter warnings

--- src/types/IsTuple.ts
+++ src/types/IsTuple.ts
@@ -10,6 +10,7 @@
  *
  * [1]: https://www.tutorialsteacher.com/typescript/typescript-tuple
  */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 export type IsTuple<T> = T extends [infer A]
   ? T
   : T extends [infer A, infer B]
@@ -21,3 +22,4 @@ export type IsTuple<T> = T extends [infer A]
   : T extends [infer A, infer B, infer C, infer D, infer E]
   ? T
   : never
+/* eslint-enable @typescript-eslint/no-unused-vars */
