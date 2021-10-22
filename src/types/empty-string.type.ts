ccf0af1ed4e041a68ceb4856f6bdfd8a2911c591
Lexus Drumgold <unicornware@flexdevelopment.llc>
2021-10-22 15:04:51 -0400

    feat(types): `EmptyString`

---
+++ src/types/empty-string.type.ts
@@ -0,0 +1,11 @@
+/**
+ * @file Type Definitions - EmptyString
+ * @module tutils/types/EmptyString
+ */
+
+/**
+ * Type representing an empty string.
+ */
+type EmptyString = ''
+
+export default EmptyString
