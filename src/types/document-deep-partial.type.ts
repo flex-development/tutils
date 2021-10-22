6fb47b7eb09303ec2a28f0c85d24230856bfbe37
Lexus Drumgold <unicornware@flexdevelopment.llc>
2021-10-22 15:17:38 -0400

    feat(types): `DocumentDeepPartial`, `DocumentPartial`

---
+++ src/types/document-deep-partial.type.ts
@@ -0,0 +1,26 @@
+import type DUID from './duid.type'
+import type ObjectPlain from './object-plain.type'
+import type ObjectUnknown from './object-unknown.type'
+import type OrDeepPartial from './or-deep-partial.type'
+import type UID from './uid.type'
+
+/**
+ * @file Type Definitions - DocumentDeepPartial
+ * @module tutils/types/DocumentDeepPartial
+ */
+
+/**
+ * Object that includes all object properties or a subset.
+ *
+ * Even when a subset of properties are requested, a partial `Document` response
+ * will always include the `id` field, or the selected uid field.
+ *
+ * @template D - Document (collection object)
+ * @template U - Name of document uid field
+ */
+type DocumentDeepPartial<
+  D extends ObjectPlain = ObjectUnknown,
+  U extends string = DUID
+> = Omit<OrDeepPartial<D>, U> & Record<U, UID>
+
+export default DocumentDeepPartial
---
+++ src/types/document-partial.type.ts
@@ -0,0 +1,26 @@
+import type DUID from './duid.type'
+import type ObjectPlain from './object-plain.type'
+import type ObjectUnknown from './object-unknown.type'
+import type OrPartial from './or-partial.type'
+import type UID from './uid.type'
+
+/**
+ * @file Type Definitions - DocumentPartial
+ * @module tutils/types/DocumentPartial
+ */
+
+/**
+ * Object that includes all object properties or a subset.
+ *
+ * Even when a subset of properties are requested, a partial `Document` response
+ * will always include the `id` field, or the selected uid field.
+ *
+ * @template D - Document (collection object)
+ * @template U - Name of document uid field
+ */
+type DocumentPartial<
+  D extends ObjectPlain = ObjectUnknown,
+  U extends string = DUID
+> = Omit<OrPartial<D>, U> & Record<U, UID>
+
+export default DocumentPartial
