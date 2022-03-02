462f102c424fe623c920cdae3d3a9cca54e0ae78
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-03-01 19:15:02 -0500

    feat(types): `Numeric`

---
+++ src/types/numeric.type.ts
@@ -0,0 +1,11 @@
+/**
+ * @file Type Definitions - Numeric
+ * @module tutils/types/Numeric
+ */
+
+/**
+ * A string that contains only numbers.
+ */
+type Numeric = `${number}`
+
+export default Numeric
