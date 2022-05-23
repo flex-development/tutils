c7a3b419db0a5146f0441be2d981988df95f1673
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-05-22 20:43:46 -0400

    chore: update `lint-staged` config comments

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -7,25 +7,24 @@ const { extensions } = require('./.vscode/settings.json')['eslint.options']
 
 module.exports = {
   /**
-   * Attempt to fix code style when JavaScript, Markdown, and TypeScript files
-   * are changed.
+   * Attempt to fix code style.
    */
   [`*.{${extensions.join(',')}}`]: ['yarn fix:style', 'git add -A'],
 
   /**
-   * Attempt to fix formatting when **ANY** file is changed.
+   * Attempt to fix formatting.
    */
   '*': ['yarn fix:format', 'git add -A'],
 
   /**
-   * Run type check when TypeScript files are changed.
+   * Run type check.
    *
    * @return {string} Type check command
    */
   '{**/*.ts,**/tsconfig.*}': [() => 'yarn check:types'],
 
   /**
-   * Run local integrity check when dependencies are updated.
+   * Run local integrity check.
    */
   'yarn.lock': ['yarn check:dedupe']
 }
