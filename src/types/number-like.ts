a7af5d6b7195c545b0e03890302b54f8590b2927
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-08-16 19:47:38 -0400

    feat(types): `NumberLike`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/types/number-like.ts
@@ -0,0 +1,13 @@
+/**
+ * @file Type Definitions - NumberLike
+ * @module tutils/types/NumberLike
+ */
+
+import type Numeric from './numeric'
+
+/**
+ * A string that contains only numbers or a number.
+ */
+type NumberLike = Numeric | number
+
+export { type NumberLike as default }
