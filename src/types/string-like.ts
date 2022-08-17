c01856eb036acf762487cc9ccf5de2f9539b10a1
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-08-16 21:05:16 -0400

    feat(types): `StringLike`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/types/string-like.ts
@@ -0,0 +1,11 @@
+/**
+ * @file Type Definitions - StringLike
+ * @module tutils/types/StringLike
+ */
+
+/**
+ * A value that has a `toString()` function.
+ */
+type StringLike = { toString(): string }
+
+export { type StringLike as default }
