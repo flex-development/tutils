2c95cd11efdc9446e6a77d4030d9ceb15c82b090
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-08-16 21:04:14 -0400

    feat(types): `ContinuousValue`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/types/continuous-value.ts
@@ -0,0 +1,13 @@
+/**
+ * @file Type Definitions - ContinuousValue
+ * @module tutils/types/ContinuousValue
+ */
+
+/**
+ * [Continuous][1] data types.
+ *
+ * [1]: https://mathsisfun.com/definitions/continuous-data
+ */
+type ContinuousValue = Date | number
+
+export { type ContinuousValue as default }
