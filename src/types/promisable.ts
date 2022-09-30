a6f25d19f1dc58414365b27f0dbaa66514f03712
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-09-30 18:55:10 -0400

    refactor(types)!: `OrPromise` -> `Promisable`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/types/promisable.ts
@@ -0,0 +1,15 @@
+/**
+ * @file Type Definitions - Promisable
+ * @module tutils/types/Promisable
+ */
+
+/**
+ * Type representing an awaitable or synchronous value.
+ *
+ * @see https://github.com/microsoft/TypeScript/issues/31394
+ *
+ * @template T - Value type
+ */
+type Promisable<T> = PromiseLike<T> | T
+
+export { type Promisable as default }
