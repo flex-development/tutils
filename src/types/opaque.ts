5a6fe9b563ef0bca722babe94ee7d7c888d8fd80
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-11-23 04:20:25 -0500

    feat(types): `Opaque`
    - https://codemix.com/opaque-types-in-javascript/
    
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/types/opaque.ts
@@ -0,0 +1,24 @@
+/**
+ * @file Type Definitions - Opaque
+ * @module tutils/types/Opaque
+ */
+
+declare const tag: unique symbol
+
+/**
+ * Creates an [opaque type][1].
+ *
+ * An opaque type hides its internal details and can only be created by being
+ * used explicitly.
+ *
+ * The type token parameter, `T`, allows TypeScript to differentiate between
+ * opaque types with the same base type.
+ *
+ * [1]: https://codemix.com/opaque-types-in-javascript/
+ *
+ * @template B - Base type
+ * @template T - Type token
+ */
+type Opaque<B, T = unknown> = B & { readonly [tag]: T }
+
+export { type Opaque as default }
