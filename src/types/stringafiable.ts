98599eb63a7c61051e515212699060d5541d8a01
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-01-31 00:32:11 -0500

    refactor(types)!: `StringLike` -> `Stringafiable`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/types/stringafiable.ts
@@ -0,0 +1,11 @@
+/**
+ * @file Type Definitions - Stringafiable
+ * @module tutils/types/Stringafiable
+ */
+
+/**
+ * A value that has a `toString()` function.
+ */
+type Stringafiable = { toString(): string }
+
+export type { Stringafiable as default }
