5185d7b3c5974a4dc9c08edce931aca2acef2386
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-02-01 17:27:39 -0500

    refactor(enums)!: remove `BsonTypeAlias`, `BsonTypeCode`
    - removed in favor of being moved to new package
    
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -4,8 +4,6 @@
  */
 
 export { default as AppEnv } from './app-env'
-export { default as BsonTypeAlias } from './bson-type-alias'
-export { default as BsonTypeCode } from './bson-type-code'
 export { default as CompareResult } from './compare-result'
 export { default as HttpStatus } from './http-status'
 export { default as NodeEnv } from './node-env'
