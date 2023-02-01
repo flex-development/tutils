922d7f8dbb7b9c954d96670efa1d0bce0c3ca50e
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-02-01 16:06:15 -0500

    test(guards): [`isInt`] stabilize tests
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/guards/__tests__/is-int.spec.ts
+++ src/guards/__tests__/is-int.spec.ts
@@ -3,14 +3,16 @@
  * @module tutils/guards/tests/unit/isInt
  */
 
+import FLOAT from '#fixtures/float'
+import INT from '#fixtures/int'
 import testSubject from '../is-int'
 
 describe('unit:guards/isInt', () => {
   it('should return false if value is not an integer', () => {
-    expect(testSubject(faker.number.float({ min: 0.13 }))).to.be.false
+    expect(testSubject(FLOAT)).to.be.false
   })
 
   it('should return true if value is an integer', () => {
-    expect(testSubject(faker.number.int())).to.be.true
+    expect(testSubject(INT)).to.be.true
   })
 })
