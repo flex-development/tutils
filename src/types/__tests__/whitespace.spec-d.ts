bd67b751c00c8062e35dd7e1de3dbe85d1e61fb7
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-05-16 15:43:19 -0400

    test(types): [`Whitespace`] fix test name formatting
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/types/__tests__/whitespace.spec-d.ts
+++ src/types/__tests__/whitespace.spec-d.ts
@@ -6,107 +6,107 @@
 import type TestSubject from '../whitespace'
 
 describe('unit-d:types/Whitespace', () => {
-  it('should extract "\u{9}"', () => {
+  it('should extract "\\u{9}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{9}'>().not.toBeNever()
   })
 
-  it('should extract "\u{20}"', () => {
+  it('should extract "\\u{20}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{20}'>().not.toBeNever()
   })
 
-  it('should extract "\u{85}"', () => {
+  it('should extract "\\u{85}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{85}'>().not.toBeNever()
   })
 
-  it('should extract "\u{200A}"', () => {
+  it('should extract "\\u{200A}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{200A}'>().not.toBeNever()
   })
 
-  it('should extract "\u{202F}"', () => {
+  it('should extract "\\u{202F}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{202F}'>().not.toBeNever()
   })
 
-  it('should extract "\u{205F}"', () => {
+  it('should extract "\\u{205F}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{205F}'>().not.toBeNever()
   })
 
-  it('should extract "\u{1680}"', () => {
+  it('should extract "\\u{1680}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{1680}'>().not.toBeNever()
   })
 
-  it('should extract "\u{2000}"', () => {
+  it('should extract "\\u{2000}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{2000}'>().not.toBeNever()
   })
 
-  it('should extract "\u{2001}"', () => {
+  it('should extract "\\u{2001}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{2001}'>().not.toBeNever()
   })
 
-  it('should extract "\u{2002}"', () => {
+  it('should extract "\\u{2002}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{2002}'>().not.toBeNever()
   })
 
-  it('should extract "\u{2003}"', () => {
+  it('should extract "\\u{2003}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{2003}'>().not.toBeNever()
   })
 
-  it('should extract "\u{2004}"', () => {
+  it('should extract "\\u{2004}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{2004}'>().not.toBeNever()
   })
 
-  it('should extract "\u{2005}"', () => {
+  it('should extract "\\u{2005}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{2005}'>().not.toBeNever()
   })
 
-  it('should extract "\u{2006}"', () => {
+  it('should extract "\\u{2006}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{2006}'>().not.toBeNever()
   })
 
-  it('should extract "\u{2007}"', () => {
+  it('should extract "\\u{2007}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{2007}'>().not.toBeNever()
   })
 
-  it('should extract "\u{2008}"', () => {
+  it('should extract "\\u{2008}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{2008}'>().not.toBeNever()
   })
 
-  it('should extract "\u{2009}"', () => {
+  it('should extract "\\u{2009}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{2009}'>().not.toBeNever()
   })
 
-  it('should extract "\u{2028}"', () => {
+  it('should extract "\\u{2028}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{2028}'>().not.toBeNever()
   })
 
-  it('should extract "\u{2029}"', () => {
+  it('should extract "\\u{2029}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{2029}'>().not.toBeNever()
   })
 
-  it('should extract "\u{3000}"', () => {
+  it('should extract "\\u{3000}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{3000}'>().not.toBeNever()
   })
 
-  it('should extract "\u{A}"', () => {
+  it('should extract "\\u{A}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{A}'>().not.toBeNever()
   })
 
-  it('should extract "\u{A0}"', () => {
+  it('should extract "\\u{A0}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{A0}'>().not.toBeNever()
   })
 
-  it('should extract "\u{B}"', () => {
+  it('should extract "\\u{B}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{B}'>().not.toBeNever()
   })
 
-  it('should extract "\u{C}"', () => {
+  it('should extract "\\u{C}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{C}'>().not.toBeNever()
   })
 
-  it('should extract "\u{D}"', () => {
+  it('should extract "\\u{D}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{D}'>().not.toBeNever()
   })
 
-  it('should extract "\u{FEFF}"', () => {
+  it('should extract "\\u{FEFF}"', () => {
     expectTypeOf<TestSubject>().extract<'\u{FEFF}'>().not.toBeNever()
   })
 })
