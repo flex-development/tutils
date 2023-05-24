5a96c82250af7c89f3f630a5040d10cec89ced48
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-05-24 02:13:54 -0400

    refactor(types): [`Predicate`] use `Fn`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/types/predicate.ts
+++ src/types/predicate.ts
@@ -3,17 +3,19 @@
  * @module tutils/types/Predicate
  */
 
+import type Fn from './fn'
+
 /**
- * Function that is called once per each element in an array until an array item
- * is found where the predicate function returns `true`.
+ * Function that is called once per each item in `array` and returns a boolean
+ * indicating if `item` meets a certain condition.
  *
  * @template T - Array item type
  *
  * @param {T} item - Current array item
- * @param {number} index - Index of `item` in `arr`
- * @param {T[]} arr - Original array
+ * @param {number} index - Index of `item` in `array`
+ * @param {T[]} array - Original array
  * @return {boolean} `true` when condition is met, `false` otherwise
  */
-type Predicate<T = unknown> = (item: T, index: number, arr: T[]) => boolean
+type Predicate<T = unknown> = Fn<[T, number, T[]], boolean>
 
 export type { Predicate as default }
