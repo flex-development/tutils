5a6e803e5afc2ebf7fcd862d409c9e35b0e060d5
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-08-04 19:07:13 -0400

    build(esm)!: pure esm
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # tutils
 
-[![github release](https://img.shields.io/github/v/release/flex-development/tutils.svg?include_prereleases&sort=semver)](https://github.com/flex-development/tutils/releases/latest)
+[![github release](https://img.shields.io/github/v/release/flex-development/tutils.svg?include_prereleases)](https://github.com/flex-development/tutils/releases/latest)
 [![npm](https://img.shields.io/npm/v/@flex-development/tutils.svg)](https://npmjs.com/package/@flex-development/tutils)
 [![codecov](https://codecov.io/gh/flex-development/tutils/branch/main/graph/badge.svg?token=ZT857Z0ITA)](https://codecov.io/gh/flex-development/tutils)
-[![module type: cjs+esm](https://img.shields.io/badge/module%20type-cjs%2Besm-brightgreen)](https://github.com/voxpelli/badges-cjs-esm)
+[![module type: esm](https://img.shields.io/badge/module%20type-esm-brightgreen)](https://github.com/voxpelli/badges-cjs-esm)
 [![license](https://img.shields.io/github/license/flex-development/tutils.svg)](LICENSE.md)
 [![conventional commits](https://img.shields.io/badge/-conventional%20commits-fe5196?logo=conventional-commits&logoColor=ffffff)](https://conventionalcommits.org/)
 [![typescript](https://img.shields.io/badge/-typescript-3178c6?logo=typescript&logoColor=ffffff)](https://typescriptlang.org/)
@@ -30,6 +30,8 @@ This package contains a set of [TypeScript][1]-friendly utilities.
 
 ## Install
 
+This package is [ESM only][2].
+
 ```sh
 yarn add @flex-development/tutils
 ```
@@ -53,9 +55,13 @@ yarn add @flex-development/tutils@flex-development/tutils
 
 ## Related
 
-- [`pkg-types`][2] - TypeScript definitions for `package.json`
-- [`tsconfig-types`][3] - TypeScript definitions for `tsconfig.json`
+- [`esm-types`][3] - TypeScript definitions for [ECMAScript modules][4]
+- [`pkg-types`][5] - TypeScript definitions for `package.json`
+- [`tsconfig-types`][6] - TypeScript definitions for `tsconfig.json`
 
 [1]: https://typescriptlang.org/
-[2]: https://github.com/flex-development/pkg-types
-[3]: https://github.com/flex-development/tsconfig-types
+[2]: https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c
+[3]: https://github.com/flex-development/esm-types
+[4]: https://nodejs.org/api/esm.html
+[5]: https://github.com/flex-development/pkg-types
+[6]: https://github.com/flex-development/tsconfig-types
--- build.config.ts
+++ build.config.ts
@@ -4,7 +4,13 @@
  * @see https://github.com/flex-development/mkbuild
  */
 
-import { defineBuildConfig, type Config } from '@flex-development/mkbuild'
+import {
+  defineBuildConfig,
+  type Config,
+  type OutputMetadata
+} from '@flex-development/mkbuild'
+import pathe from '@flex-development/pathe'
+import type { BuildResult, OutputFile, PluginBuild } from 'esbuild'
 import pkg from './package.json' assert { type: 'json' }
 import tsconfig from './tsconfig.build.json' assert { type: 'json' }
 
@@ -15,9 +21,96 @@ import tsconfig from './tsconfig.build.json' assert { type: 'json' }
  */
 const config: Config = defineBuildConfig({
   charset: 'utf8',
-  entries: [{ format: 'esm' }, { format: 'cjs' }],
-  sourcemap: true,
-  sourcesContent: false,
+  entries: [
+    { dts: 'only' },
+    { dts: false, pattern: ['**/index.ts', 'enums/*'] },
+    {
+      dts: false,
+      pattern: ['!utils/*.options.ts', 'index.ts', 'utils/*'],
+      sourceRoot: pathe.join(pkg.homepage, 'tree', pkg.tagPrefix + pkg.version),
+      sourcemap: true,
+      sourcesContent: true
+    }
+  ],
+  minifySyntax: true,
+  plugins: [
+    {
+      /**
+       * Plugin name.
+       *
+       * @const {string} name
+       */
+      name: 'fix-sourcemaps',
+
+      /**
+       * Makes sourcemap files relative to [`sourceRoot`][1].
+       *
+       * [1]: https://esbuild.github.io/api/#source-root
+       * [2]: https://esbuild.github.io/plugins
+       *
+       * @see https://github.com/evanw/esbuild/issues/2218
+       *
+       * @param {PluginBuild} build - [esbuild plugin api][2]
+       * @param {PluginBuild['onEnd']} build.onEnd - Build end callback
+       * @return {void} Nothing when complete
+       */
+      setup({ initialOptions, onEnd }: PluginBuild): void {
+        const { absWorkingDir = process.cwd() } = initialOptions
+
+        return void onEnd((result: BuildResult): void => {
+          /**
+           * Output file objects.
+           *
+           * @const {OutputFile[]} outputFiles
+           */
+          const outputFiles: OutputFile[] = []
+
+          for (const output of result.outputFiles!) {
+            if (output.path.endsWith('.map')) {
+              /**
+               * Relative path to output file sourcemap is for.
+               *
+               * **Note**: Relative to {@linkcode absWorkingDir}.
+               *
+               * @const {string} outfile
+               */
+              const outfile: string = output.path
+                .replace(absWorkingDir, '')
+                .replace(/^\//, '')
+                .replace(/\.map$/, '')
+
+              /**
+               * Output metadata for {@linkcode outfile}.
+               *
+               * @const {OutputMetadata} metadata
+               */
+              const metadata: OutputMetadata =
+                result.metafile!.outputs[outfile]!
+
+              /**
+               * Parsed sourcemap object.
+               *
+               * @const {{ sources: string[] }}
+               */
+              const map: { sources: string[] } = JSON.parse(output.text)
+
+              // reset sources to outfile entry point
+              map.sources = [metadata.entryPoint!]
+
+              // redefine outfile text
+              Object.defineProperty(output, 'text', {
+                get: (): string => JSON.stringify(map, null, 2)
+              })
+            }
+
+            outputFiles.push(output)
+          }
+
+          return void (result.outputFiles = outputFiles)
+        })
+      }
+    }
+  ],
   target: [
     pkg.engines.node.replace(/^\D+/, 'node'),
     tsconfig.compilerOptions.target
